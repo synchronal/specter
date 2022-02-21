6434106e7902a183268ce645c5122a20eff91390
Eric Saxby <sax@livinginthepast.org>
2022-02-21 08:52:11 -0800

    Add specs

--- lib/specter/native.ex
+++ lib/specter/native.ex
@@ -2,6 +2,12 @@ defmodule Specter.Native do
   @moduledoc false
   use Rustler, otp_app: :specter, crate: :specter_nif
 
-  @spec init() :: {:ok, reference()}
-  def init, do: :erlang.nif_error(:nif_not_loaded)
+  @opaque t() :: reference()
+
+  @type uuid() :: String.t()
+
+  @spec init() :: {:ok, t()}
+  def init, do: error()
+
+  defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
