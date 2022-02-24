b555477042ac7d9a4b3a4dd2dd245a4147cfa313
Eric Saxby <sax@livinginthepast.org>
2022-02-24 11:46:08 -0800

    Add medic

---
+++ .medic/doctor.exs
@@ -0,0 +1,6 @@
+[
+  {Medic.Checks.Asdf, :plugin_installed?, ["rust"]},
+  {Medic.Checks.Hex, :local_hex_installed?},
+  {Medic.Checks.Hex, :local_rebar_installed?},
+  {Medic.Checks.Hex, :packages_installed?}
+]
---
+++ .medic/require.exs
@@ -0,0 +1,3 @@
+Mix.install([
+  {:medic, "~> 1.5.1", force: true}
+])
